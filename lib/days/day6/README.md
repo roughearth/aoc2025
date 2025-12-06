1fe4374061dbf1eb0fa8e53071100bdaca779bae
Richard Hubbard <richard.hubbard@armadry.co.uk>
2025-12-06 15:11:35 +0000

    Day 6

--- lib/days/day6/README.md
+++ lib/days/day6/README.md
@@ -1,3 +1,5 @@
 # Day 6
 
+This is the first time in a while (if ever) that I have needed either to parse the input differently for part 1 and part 2, or to parse it withouth using my `clearAndParse` utility.
 
+Interesting, but still reasonably straightforward.
--- lib/days/day6/index.ts
+++ lib/days/day6/index.ts
@@ -1,16 +1,97 @@
 import { eg, input } from './input';
-import { cleanAndParse } from '../../utils';
+import { cleanAndParse, sumOf } from '../../utils';
 import { Day } from '..';
 
 export const meta: Day['meta'] = {};
 
+type Problem = {
+  numbers: number[];
+  operator: string;
+};
+
+function processProblems(problems: Problem[]) {
+  const results = problems.map(({ numbers, operator }) => {
+    if (operator === "*") {
+      return numbers.reduce((a, b) => a * b, 1);
+    }
+    else {
+      return numbers.reduce((a, b) => a + b, 0);
+    }
+  });
+  return results;
+}
+
 export function part1() {
-  return 0;
+  const numbers: number[][] = [];
+  let operators: string[] = [];
+  const problems: Problem[] = [];
+
+  cleanAndParse(input, (line) => {
+    //amount of whitespace is irrelevant here
+    const src = line.trim().split(/\s+/);
+
+    if (src[0] === "*") { // true for my input and the example
+      operators = src;
+    }
+    else {
+      numbers.push(src.map(Number));
+    }
+  });
+
+  const l = operators.length;
+
+  for (let i = 0; i < l; i++) {
+    problems.push({
+      numbers: numbers.map((n) => n[i]),
+      operator: operators[i]
+    });
+  }
+
+  return sumOf(processProblems(problems));
 }
 
 export function part2() {
-  return 0;
+  const problems: Problem[] = [];
+
+  const lines = input.split("\n");
+  const numberLines = lines.slice(0, -1);
+
+  // amount of whitespace is still irrelevant on the operator line
+  const operators = lines.at(-1)!.trim().split(/\s+/)
+
+  // this gets replaced when the problem changes
+  let currentNumbers: number[] = [];
+
+  // number of character columns to loop over
+  const len = Math.max(...lines.map((l) => l.length));
+
+  for (let i = 0; i < len; i++) {
+    const number = numberLines.map(line => line[i] ?? "").join("").trim();
+
+    if (number === "") {
+      // a blank column indicates a new problem, so push and reset
+      problems.push({
+        numbers: currentNumbers,
+        operator: operators[problems.length]
+      });
+
+      currentNumbers = [];
+    }
+    else {
+      currentNumbers.push(Number(number));
+    }
+  }
+
+  // don't forget to push the last problem
+  problems.push({
+    numbers: currentNumbers,
+    operator: operators[problems.length]
+  });
+
+  return sumOf(processProblems(problems));
 }
 
-// export const answers = [
-// ];
+export const answers = [
+  5227286044585,
+  10227753257799
+];
