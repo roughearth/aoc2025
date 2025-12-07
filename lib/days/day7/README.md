7280b7002d71a5bcd25a1b268b4a364a0feeb5fc
Richard Hubbard <richard.hubbard@armadry.co.uk>
2025-12-07 12:23:23 +0000

    Day 7

--- lib/days/day7/README.md
+++ lib/days/day7/README.md
@@ -1,3 +1,13 @@
 # Day 7
 
 
+Oooh, _another_ day without using `cleanAndParse`!
+
+Parts 2 stumped me for a bit. I got hung up on making a graph and DFSing it.
+
+The logic goes like this.
+
+* start with the source beam, which only has 1 route to itself.
+* Go down the grid row by row, and every time you hit a splitter, drop that beam and make 2 new ones.
+  * Here's the key, if there is already a beam in that column, add the number of routes from the beam being split to the existing beam. This makes sure that combined beams get appropriately weighted.
+  * Also, count the number of times you split a beam for part 1.
--- lib/days/day7/index.ts
+++ lib/days/day7/index.ts
@@ -1,16 +1,45 @@
 import { eg, input } from './input';
-import { cleanAndParse } from '../../utils';
+import { cleanAndParse, sumOf } from '../../utils';
 import { Day } from '..';
 
 export const meta: Day['meta'] = {};
 
+function parseBeams(input: string) {
+  const splitter = input.split('\n');
+  const finalRowIndex = splitter.length - 1;
+
+  const beams = new Map([[splitter[0].indexOf('S'), 1]]);
+  let splitCount = 0;
+
+
+  for (let currentRow = 1; currentRow < finalRowIndex; currentRow++) {
+    for (const [col, count] of beams) {
+      const row = splitter[currentRow];
+      if (row[col] === '^') {
+        beams.delete(col);
+        beams.set(col - 1, count + (beams.get(col - 1) || 0));
+        beams.set(col + 1, count + (beams.get(col + 1) || 0));
+        splitCount++;
+      }
+    }
+  }
+
+  return { beams, splitCount };
+}
+
 export function part1() {
-  return 0;
+  const { splitCount } = parseBeams(input);
+
+  return splitCount;
 }
 
 export function part2() {
-  return 0;
+  const { beams } = parseBeams(input);
+
+  return sumOf(Array.from(beams.values()));
 }
 
-// export const answers = [
-// ];
+export const answers = [
+  1656,
+  76624086587804
+];
